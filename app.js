fce8e775cebc85a883e776fef28ca3d6bceb4d16
gursahib-badwal <gursahibs156@gmail.com>
2023-03-19 09:34:03 -0700

    Express

--- app.js
+++ app.js
@@ -260,6 +260,16 @@
 // console.log(second);
 // console.log("Oye pols aa gyi pols!!");
 
+// -----------------------------------------------------------------------------------------
+// Express
+const express = require("express");
+const app = express();
 
+app.get('/', (req,res) => {
+    res.send("Home Page");
+})
 
+app.listen("5000", ()=> {
+    console.log("The server is listening on port 5000...");
+})
 
